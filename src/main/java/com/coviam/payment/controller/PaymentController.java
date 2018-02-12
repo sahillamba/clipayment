4cdc2c1e3df3df4164bbb4cf78f4522407c2a0c1
aditya8083 <apandey8083@gmail.com>
2018-02-12 20:07:38 +0530

    api changes commit

--- src/main/java/com/coviam/payment/controller/PaymentController.java
+++ src/main/java/com/coviam/payment/controller/PaymentController.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,6 +17,8 @@ import java.util.List;
  * Created by avinashkumar on 05/02/2018 AD.
  */
 @RestController
+@CrossOrigin("*")
+@RequestMapping("/payment")
 public class PaymentController {
 
     @Autowired
--- src/main/java/com/coviam/payment/controller/ProviderController.java
+++ src/main/java/com/coviam/payment/controller/ProviderController.java
@@ -8,17 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by avinashkumar on 06/02/2018 AD.
  */
 @RestController
-@RequestMapping("/provider")
+@CrossOrigin("*")
+@RequestMapping("/payment/provider")
 public class ProviderController {
 
     @Autowired
